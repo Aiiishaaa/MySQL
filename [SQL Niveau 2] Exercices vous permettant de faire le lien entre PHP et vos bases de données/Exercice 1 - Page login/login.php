609f860570db0e3b02e2e0098237a88cc947af7e
Aiiishaaa <aicha.hamida06@yahoo.fr>
2020-10-19 15:04:37 +0200

    2nd Commit

--- [SQL Niveau 2] Exercices vous permettant de faire le lien entre PHP et vos bases de données/Exercice 1 - Page login/login.php
+++ [SQL Niveau 2] Exercices vous permettant de faire le lien entre PHP et vos bases de données/Exercice 1 - Page login/login.php
@@ -1,27 +1,33 @@
+
 <?php
+       
+        try{
+            $bdd =new PDO('mysql:host=localhost; dbname=connection; charset=utf8', 'root', '');
+            // Activation des erreurs PDO
+            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // mode de fetch par défaut : FETCH_ASSOC / FETCH_OBJ / FETCH_BOTH
+            $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            } 
+        catch(PDOException $e) {
+                die('Erreur : ' . $e->getMessage());
+            }
 
-if (isset($_POST["submit"])) {
+    //préparation de la requête et des variables
+                    $sql = "INSERT INTO connexions (login,password) VALUES (:login,:password)";
 
-    $login = htmlspecialchars($_POST["login"]);
-    $isValidLogin = !empty($login);
-
-    $password = htmlspecialchars($_POST["password"]);
-    $isValidPassword = !empty($password);
-
-    $isValidAll = $isValidLogin && $isValidPassword;
-
-    if ($isValidAll) {
-        try {
-            $database = new PDO("mysql:host=localhost;dbname=nv2_mysql-php;charset=utf8", "root", "");
-        } catch (Exception $err) {
-            die("Erreur:" . $err->getMessage());
-        }
-        $insertReq = $database->prepare("INSERT INTO connexions (login, password) VALUES (:login, :password)");
-        $insertReq->bindValue(':login', $login);
-        $insertReq->bindValue(':password', $password);
-        $insertReq->execute();
-    }
-}
+                    $datas = array(':login'=>$_POST['login'], ':password'=>$_POST['password']);
+                     //Execution de la requête
+                try{
+                            $requete = $bdd -> prepare($sql) ;
+                            $requete->execute($datas) ;
+                }
+                catch(Exception $e){
+                        // en cas d'erreur :
+                        echo " Erreur ! ".$e->getMessage();
+                        echo " Les datas : " ;
+                        print_r($datas);
+                }
+?>
 
 ?>
 
@@ -36,17 +42,21 @@ if (isset($_POST["submit"])) {
 
 <body>
     <form class="form__group" action="login.php" method="post">
+
         <div class="form__group">
             <label for="login" class="form__label">Login :</label>
             <input type="text" name="login" id="login" class="form__input">
         </div>
+
         <div class="form__group">
             <label for="password" class="form__label">Mot de passe :</label>
             <input type="text" name="password" id="password" class="form__input">
         </div>
+
         <div class="form__group">
             <input type="submit" value="Envoyer" name="submit" class="form__input">
         </div>
+
     </form>
 </body>
 
