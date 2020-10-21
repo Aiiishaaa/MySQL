3d898b28b977e66521e5ae10501e5fca77626364
Aiiishaaa <aicha.hamida06@yahoo.fr>
2020-10-21 11:01:08 +0200

    2nd Commit

--- [SQL Niveau 2] Exercices vous permettant de faire le lien entre PHP et vos bases de données/Exercice 2 - Page signin/signin.php
+++ [SQL Niveau 2] Exercices vous permettant de faire le lien entre PHP et vos bases de données/Exercice 2 - Page signin/signin.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>signin</title>
+    <title>Sign in</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
-    
+
     form{
         margin-left: 300px;
         margin-top: 100px;
@@ -39,6 +39,9 @@ $isValidlastName = !empty($lastName);
 $password = htmlspecialchars($_POST["password"]);
 $isValidpwd = !empty($password);
 
+$confirmpassword = htmlspecialchars($_POST["confirmpassword"]);
+$isValidconfirmpwd = !empty($confirmpassword);
+
 $email = htmlspecialchars($_POST["email"]);
 $isValidemail = !empty($email);
 
@@ -46,32 +49,40 @@ $info = htmlspecialchars($_POST["info"]);
 $isValidinfo = !empty($info);
 
 
-if (isset($_POST['confirm'])){
+if (isset($_POST['confirm'])) {
     $condition = $_POST['condition'];
     if (empty($condition)){
         echo '<script type="text/javascript">';
-        echo 'alert("Vous devez accepter les conditions")'; 
+        echo 'alert("Vous devez accepter les conditions !")'; 
         echo '</script>';
     }
-    else{
-        if( $isValidfirstName && $isValidlastName && $isValidemail && $isValidpwd && $isValidinfo)
-        {
-            $firstName = $_POST['firstName'];
-            $lastName = $_POST['lastName'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $info = $_POST['info'];
+    else{ 
+        if( $isValidfirstName && $isValidlastName && $isValidemail && $isValidpwd && $isValidinfo && $isValidconfirmpwd ){ 
+                    if ($password === $confirmpassword ) {
+                        $firstName = $_POST['firstName'];
+                        $lastName = $_POST['lastName'];
+                        $email = $_POST['email'];
+                        $password = $_POST['password'];
+                        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-            $base = new PDO('mysql:host=localhost;dbname=connection;charset=utf8', 'root', '');
-            $sql = "INSERT INTO users(firstName,lastName,password,email,info)VALUES('$firstName','$lastName','$password','$email','$info')"; 
-            $base -> query($sql);
+                        $info = $_POST['info'];
+
+                        $base = new PDO('mysql:host=localhost;dbname=connection;charset=utf8', 'root', '');
+                        $sql = "INSERT INTO users(firstName,lastName,password,email,info)VALUES('$firstName','$lastName','$passwordHash','$email','$info')"; 
+                        $base -> query($sql);
+                    } 
+                    else {
+                                echo '<script type="text/javascript">';
+                                echo 'alert("les mots de passe saisis ne sont pas identiques")';
+                                echo  '</script>';
+                        
+                    }
         }
         else{
         echo '<script type="text/javascript">';
-        echo 'alert("ERROR")';
+        echo 'alert("Tous les champs sont obligatoires !")';
         echo  '</script>';
-
-    }
+        }
     }
 }
     
@@ -96,8 +107,8 @@ if (isset($_POST['confirm'])){
         <input type="text" name="password" id="ipassword">
     </div>
     <div class="form-group">
-        <label for="ipassword"> Confirmation :</label>
-        <input type="text" name="password2">
+        <label for="iconfirmpassword"> Confirmation :</label>
+        <input type="text" name="confirmpassword">
     </div>
     <div class="form-group">
         <label for="imail">    Email :</label>
